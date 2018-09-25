79d6273be3f8bd981eabb9bf48dba43e5bc9de04
DenisKruglik <d.kruglik11011@gmail.com>
2018-09-25 14:16:14 +0300

    Copleted Quicksort and tests

---
+++ Quicksort/Sorting.Tests/Sorting.Tests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sorting.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Quicksort_RegularIntArrayPassed_ArrayIsSorted()
+        {
+            int[] source = new[] {3, 1, 5, 4, 9, 6, 2, 7, 8};
+
+            var p = 0;
+
+            var r = source.Length - 1;
+
+            int[] expected = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            
+            Sorting.Quicksort(source, p, r);
+            
+            Assert.AreEqual(expected, source);
+        }
+
+        [Test]
+        public void Quicksort_BoundOfPartPassed_ArrayIsPartiallySorted()
+        {
+            int[] source = new[] {6, 1, 9, 3, 2, 0, 4, 5, 8, 7};
+
+            var p = 2;
+
+            var r = 5;
+
+            int[] expected = new[] {6, 1, 0, 2, 3, 9, 4, 5, 8, 7};
+            
+            Sorting.Quicksort(source, p, r);
+            
+            Assert.AreEqual(expected, source);
+        }
+
+        [Test]
+        public void Quicksort_LowIsGreaterThanHigh_ArrayStaysTheSame()
+        {
+            int[] source = new[] {6, 1, 9, 3, 2, 0, 4, 5, 8, 7};
+
+            var p = 5;
+
+            var r = 2;
+
+            int[] expected = (int[])source.Clone();
+            
+            Sorting.Quicksort(source, p, r);
+            
+            Assert.AreEqual(expected, source);
+        }
+
+        [Test]
+        public void Quicksort_BoundsOutOfBounds_ThrowsArgumentException()
+        {
+            int[] source = new[] {6, 1, 9, 3, 2, 0, 4, 5, 8, 7};
+
+            Assert.Catch<ArgumentException>(() => Sorting.Quicksort(source, 0, 50));
+        }
+    }
+}
---
+++ Quicksort/Sorting/Sorting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Sorting
+{
+    public class Sorting
+    {
+        public static void Quicksort(int[] arr, int p, int r)
+        {
+            if (p < 0 || r >= arr.Length)
+            {
+                throw new ArgumentException("Bounds passed are out of array bounds");
+            }
+            if (p < r)
+            {
+                var q = Partition(arr, p, r);
+                Quicksort(arr, p, q-1);
+                Quicksort(arr, q+1, r);
+            }
+        }
+
+        private static int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];  
+          
+            int i = low - 1;  
+            for (int j = low; j < high; j++) 
+            { 
+                if (arr[j] <= pivot) 
+                { 
+                    i++; 
+                    int temp = arr[i]; 
+                    arr[i] = arr[j]; 
+                    arr[j] = temp; 
+                } 
+            } 
+            
+            int temp1 = arr[i+1]; 
+            arr[i+1] = arr[high]; 
+            arr[high] = temp1; 
+  
+            return i+1;
+        }
+    }
+}
